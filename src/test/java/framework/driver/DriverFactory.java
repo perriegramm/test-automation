b7b1cfb8c70cb115c8bd1652a7321bf3786df943
perrie_gramm <agnploskonka@gmail.com>
2026-02-11 19:04:14 +0100

    feat: createDriver() refactored

--- src/test/java/framework/driver/DriverFactory.java
+++ src/test/java/framework/driver/DriverFactory.java
@@ -5,6 +5,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Objects;
+
 public class DriverFactory {
 
     private DriverFactory() {
@@ -12,13 +14,15 @@ public class DriverFactory {
     }
 
     public static WebDriver createDriver(BrowserType browserType) {
+
+        Objects.requireNonNull(browserType, "Browser type must not be null");
+
         return switch (browserType) {
             case CHROME -> {
                 WebDriverManager.chromedriver().setup();
                 yield new ChromeDriver(ChromeOptionsFactory.createDefault());
             }
             case FIREFOX -> throw new UnsupportedOperationException("Firefox not implemented yet");
-            default -> throw new IllegalArgumentException("Unsupported browser type: " + browserType);
         };
     }
 }
