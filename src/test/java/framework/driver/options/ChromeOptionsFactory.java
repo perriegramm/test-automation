d2c218c50280d46f99f51ac8a794bc23df8e1e0d
perrie_gramm <agnploskonka@gmail.com>
2026-02-11 18:40:35 +0100

    feat: introduce driver configuration layer (browser enum + chrome options factory)

---
+++ src/test/java/framework/driver/options/ChromeOptionsFactory.java
@@ -0,0 +1,28 @@
+package framework.driver.options;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeOptionsFactory {
+
+    private ChromeOptionsFactory() {
+        // Blokuje tworzenie instancji - klasa utility
+    }
+
+    public static ChromeOptions createDefault() {
+        ChromeOptions options = new ChromeOptions();
+        applyBaseConfiguration(options);
+        return options;
+    }
+
+    public static ChromeOptions createHeadless() {
+        ChromeOptions options = new ChromeOptions();
+        applyBaseConfiguration(options);
+        options.addArguments("--headless=new");
+        return options;
+    }
+
+    private static void applyBaseConfiguration(ChromeOptions options) {
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--start-maximized");
+    }
+}
